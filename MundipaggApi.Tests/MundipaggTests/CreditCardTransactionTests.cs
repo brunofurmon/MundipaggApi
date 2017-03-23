283754cba74340d706d4c37f6cc1bb81db71c470
brunofurmon <brunofurmon@poli.ufrj.br>
2017-03-23 02:29:27 -0300

    Mundipagg API tests

--- MundipaggApi.Tests/MundipaggTests/CreditCardTransactionTests.cs
+++ MundipaggApi.Tests/MundipaggTests/CreditCardTransactionTests.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GatewayApiClient.DataContracts.EnumTypes;
 using GatewayApiClient.DataContracts;
 using GatewayApiClient;
-using System.Linq;
 using GatewayApiClient.EnumTypes;
 using System.Net;
+using MundipaggApi.Tests.Factories;
 
 
 namespace MundipaggApi.Tests.MundipaggTests
@@ -14,106 +12,54 @@ namespace MundipaggApi.Tests.MundipaggTests
     [TestClass]
     public class CreditCardTransactionTests
     {
-        [TestMethod]
-        public void ShouldCreateTransaction()
+        private static Guid merchantKey = Guid.Parse("85328786-8BA6-420F-9948-5352F5A183EB");
+        private static string hostUri = @"https://sandbox.mundipaggone.com";
+        private static GatewayServiceClient serviceClient;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
         {
-            // Cria a transação.
-            var transaction = new CreditCardTransaction()
-            {
-                AmountInCents = 10000,
-                CreditCard = new CreditCard()
-                {
-                    CreditCardBrand = CreditCardBrandEnum.Visa,
-                    CreditCardNumber = "4111111111111111",
-                    ExpMonth = 10,
-                    ExpYear = 22,
-                    HolderName = "LUKE SKYWALKER",
-                    SecurityCode = "123"
-                },
-                InstallmentCount = 1
-            };
-
-            // Cria requisição.
-            var createSaleRequest = new CreateSaleRequest()
-            {
-                // Adiciona a transação na requisição.
-                CreditCardTransactionCollection = new Collection<CreditCardTransaction>(
-                    new CreditCardTransaction[] { transaction }
-                ),
-                Order = new Order()
-                {
-                    OrderReference = "NumeroDoPedido"
-                }
-            };
-
-            // Coloque a sua MerchantKey aqui.
-            Guid merchantKey = Guid.Parse("85328786-8BA6-420F-9948-5352F5A183EB");
-
-            // Cria o client que enviará a transação.
-            var serviceClient = new GatewayServiceClient(
-                merchantKey, 
-                new Uri("https://sandbox.mundipaggone.com")
+            serviceClient = new GatewayServiceClient(
+                merchantKey,
+                new Uri(hostUri)
             );
+        }
 
-            // Autoriza a transação e recebe a resposta do gateway.
+        [TestMethod]
+        public void ShouldCreateExactlyOneTransaction()
+        {
+            CreateSaleRequest createSaleRequest = SaleRequestFactory.GenerateSaleRequest();
+
             var httpResponse = serviceClient.Sale.Create(createSaleRequest);
 
-            Console.WriteLine("Código retorno: {0}", httpResponse.HttpStatusCode);
-            Console.WriteLine("Chave do pedido: {0}", httpResponse.Response.OrderResult.OrderKey);
-            if (httpResponse.Response.CreditCardTransactionResultCollection != null)
-            {
-                Console.WriteLine("Status transação: {0}",
-                    httpResponse.Response.CreditCardTransactionResultCollection
-                    .FirstOrDefault()
-                    .CreditCardTransactionStatus);
-            }
+            Assert.AreEqual(HttpStatusCode.Created, httpResponse.HttpStatusCode);
+            Assert.IsTrue(httpResponse.Response.CreditCardTransactionResultCollection.Count == 1);
         }
 
         [TestMethod]
-        public void ShouldCaptureTransaction()
+        public void ShouldCaptureCreatedTransaction()
         {
-            Guid merchantKey = Guid.Parse("85328786-8BA6-420F-9948-5352F5A183EB");
-            // Chave do pedido
-            Guid orderKey = Guid.Parse("219d7581-78e2-4aa9-b708-b7c585780bfc");
+            CreateSaleRequest createSaleRequest = SaleRequestFactory.GenerateSaleRequest();
+            var creationResponse = serviceClient.Sale.Create(createSaleRequest);
+            Guid orderKey = creationResponse.Response.OrderResult.OrderKey;
 
-            // Cria o cliente para capturar as transações.
-            IGatewayServiceClient client = new GatewayServiceClient(
-                merchantKey, 
-                new Uri("https://sandbox.mundipaggone.com")
-            );
+            var httpResponse = serviceClient.Sale.Manage(ManageOperationEnum.Capture, orderKey);
 
-            // Captura as transações de cartão de crédito do pedido.
-            var httpResponse = client.Sale.Manage(ManageOperationEnum.Capture, orderKey);
-
-            if (httpResponse.HttpStatusCode == HttpStatusCode.OK
-                && httpResponse.Response.CreditCardTransactionResultCollection.Any()
-                && httpResponse.Response.CreditCardTransactionResultCollection.All(p => p.Success == true))
-            {
-                Console.WriteLine("Transações capturadas.");
-            }
+            Assert.AreEqual(HttpStatusCode.OK, httpResponse.HttpStatusCode);
+            Assert.IsTrue(httpResponse.Response.CreditCardTransactionResultCollection.Count == 1);
         }
 
         [TestMethod]
-        public void ShouldCancelTransaction()
+        public void ShouldCancelCreatedTransaction()
         {
-            Guid merchantKey = Guid.Parse("85328786-8BA6-420F-9948-5352F5A183EB");
-            // Chave do pedido
-            Guid orderKey = Guid.Parse("219d7581-78e2-4aa9-b708-b7c585780bfc");
+            CreateSaleRequest createSaleRequest = SaleRequestFactory.GenerateSaleRequest();
+            var creationResponse = serviceClient.Sale.Create(createSaleRequest);
+            Guid orderKey = creationResponse.Response.OrderResult.OrderKey;
 
-            // Cria o cliente para cancelar as transações.
-            IGatewayServiceClient client = new GatewayServiceClient(
-                merchantKey, 
-                new Uri("https://sandbox.mundipaggone.com")
-            );
+            var httpResponse = serviceClient.Sale.Manage(ManageOperationEnum.Cancel, orderKey);
 
-            // Cancela as transações de cartão de crédito do pedido.
-            var httpResponse = client.Sale.Manage(ManageOperationEnum.Cancel, orderKey);
-
-            if (httpResponse.HttpStatusCode == HttpStatusCode.OK
-                && httpResponse.Response.CreditCardTransactionResultCollection.Any()
-                && httpResponse.Response.CreditCardTransactionResultCollection.All(p => p.Success == true)) {
-                Console.WriteLine("Transações canceladas.");
-            }
+            Assert.AreEqual(HttpStatusCode.OK, httpResponse.HttpStatusCode);
+            Assert.IsTrue(httpResponse.Response.CreditCardTransactionResultCollection.Count == 1);
         }
     }
 }
