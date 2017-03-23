a18285bfd8a9d0dade6f3f2c6fc12b968972456e
brunofurmon <brunofurmon@poli.ufrj.br>
2017-03-23 01:56:47 -0300

    Unused imports

--- MundipaggApi/Controllers/CreditCardController.cs
+++ MundipaggApi/Controllers/CreditCardController.cs
@@ -6,11 +6,7 @@ using MundipaggApi.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web;
 using System.Web.Http;
-using System.Web.Http.Results;
 
 
 namespace MundipaggApi.Controllers
